b72adc0e4976875d78c102b28d9dfda97fc64be1
biduedson <112134265+biduedson@users.noreply.github.com>
2023-08-28 18:53:38 -0300

    concluido exercicio 3

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -1,15 +1,19 @@
 type Conn = {
-    username: string,
-    password: string,
-    host: string,
-    port: string,
-    dbname: string
-}
+  username: string;
+  password: string;
+  host: string;
+  port: string;
+  dbname: string;
+};
+type Connect = {
+  driver: string;
+  url: string;
+};
+const conexao = (dados: Conn): Readonly<Connect> => {
+  const { username, password, host, port, dbname } = dados;
 
-const conexao = (dados: Conn) => {
-    const { username, password, host, port, dbname } = dados;
-    return {
-        driver: 'postgres',
-        url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
-    };
-}
+  return {
+    driver: "postgres",
+    url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`,
+  };
+};
